6b31c8108a237b56f0a713a74162e137bda5226b
g2 <g2simm+bow@gmail.com>
2022-05-11 17:44:53 +0900

    Add one click register and fix observer

--- content.js
+++ content.js
@@ -1,17 +1,10 @@
-const sleepTime = 800;
+const sleepTime = 600;
 
 const sleep = waitTime => new Promise(resolve => setTimeout(resolve, waitTime));
 
-const markAllBooks = async function () {
 
-  var books = document.getElementsByClassName('group__book');
-  const bookLength = books.length;
-
-  for (let i = bookLength - 1; i >= 0; i--) {
-    await sleep(sleepTime);
-
-    const book = books[i];
-
+const addReadBook = function (book) {
+  return async function () {
     book.querySelector('.action__text').click();
     await sleep(sleepTime);
 
@@ -21,7 +14,7 @@ const markAllBooks = async function () {
     if (selectModal.getElementsByClassName('action__registration action__registration--registered').length > 0) {
       selectModal.querySelector('.window__close.js-modal-close').click();
       await sleep(sleepTime);
-      continue;
+      return;
     }
 
     var selectButton = selectModal.querySelector('.registration__item').querySelector('.modal-button');
@@ -43,7 +36,6 @@ const markAllBooks = async function () {
 
     var registerButton = registerModal.querySelector('.action__controller').querySelector('button');
 
-
     registerButton.click();
     await sleep(sleepTime);
 
@@ -54,23 +46,56 @@ const markAllBooks = async function () {
   }
 }
 
-const observer = new MutationObserver(function () {
-  var jsInitChecktimer = setInterval(checkForJS_Finish, 111);
-  function checkForJS_Finish() {
-    if (typeof SOME_GLOBAL_VAR != "undefined"
-      || document.querySelector(".content-with-header__header")
-    ) {
-      clearInterval(jsInitChecktimer);
-      var title = document.querySelector('.content-with-header__header');
 
-      const addButton = document.createElement('input');
-      addButton.classList.add('auto-read--button');
-      addButton.type = 'button';
-      addButton.value = '全てを「読んだ本」に追加';
-      addButton.addEventListener("click", markAllBooks);
-      title.appendChild(addButton);
+const markAllBooks = async function () {
+
+  var books = document.getElementsByClassName('group__book');
+  const bookLength = books.length;
+
+  for (let i = bookLength - 1; i >= 0; i--) {
+    await sleep(sleepTime);
+
+    const book = books[i];
+    await addReadBook(book)();
+  }
+}
+
+const observer = new MutationObserver(async function () {
+
+  if (!document.querySelector('.auto-read--button')) {
+    // Create All Read Button
+    var title = document.querySelector('.content-with-header__header');
+
+    const addAutoButton = document.createElement('input');
+    addAutoButton.classList.add('auto-read--button');
+    addAutoButton.type = 'button';
+    addAutoButton.value = '全てを「読んだ本」に追加';
+    addAutoButton.addEventListener("click", markAllBooks);
+    title.appendChild(addAutoButton);
+  }
+
+  if (!document.querySelector('.auto-read--one-button')) {
+
+    // Create One Read Button
+    var books = document.getElementsByClassName('group__book');
+    const bookLength = books.length;
+
+    for (let i = bookLength - 1; i >= 0; i--) {
+      const book = books[i];
+
+      const addOneButton = document.createElement('div');
+      const addOneButtonSpan = document.createElement('span');
+      addOneButtonSpan.innerHTML = '1click登録';
+      addOneButton.classList.add('auto-read--one-button');
+      addOneButton.appendChild(addOneButtonSpan)
+      addOneButton.addEventListener("click", addReadBook(book));
+      book.appendChild(addOneButton);
     }
   }
 });
 
-observer.observe(document.querySelector('head'), { childList: true, subtree: true })
+observer.observe(document.querySelector('.bm-wrapper'), { attributes: true, childList: true, subtree: true })
+
+observer.observe(document.querySelector('head'), { attributes: true, childList: true, subtree: true })
+
+
